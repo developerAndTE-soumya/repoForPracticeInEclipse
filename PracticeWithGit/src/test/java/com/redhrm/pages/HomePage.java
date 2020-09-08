9f0f9d460b21ccf86d58bac0760ce613d6fb216a
developerAndTE-soumya <sikun2008@gmail.com>
2020-09-08 14:51:24 +0530

    my first commit for practice

---
+++ PracticeWithGit/src/test/java/com/redhrm/pages/HomePage.java
@@ -0,0 +1,11 @@
+package com.redhrm.pages;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To Homepage...");
+
+	}
+
+}
